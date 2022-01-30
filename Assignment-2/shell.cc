fff001ed96fcd9b6eda64f12213dd0a233645ad2
Parth Jindal <pmjindal@gmail.com>
2022-01-30 22:33:03 +0530

    Some commit

--- Assignment-2/shell.cc
+++ Assignment-2/shell.cc
@@ -28,47 +28,31 @@ vector<string> tokenize(const string& _cmd, char delim) {
 
 class Command {
    public:
-    string init_cmd;  // original command
-    string cmd;       // command after substitution
+    string cmd;  // command after substitution
     vector<string> argv;
     int argc;
     bool bg;
     int fd_in, fd_out, fd_err;  // file descriptors
+    string fd_in_name, fd_out_name, fd_err_name;
 
     Command(const string& _cmd)
         : cmd(_cmd), bg(false), fd_in(0), fd_out(1), fd_err(2) {
-        init_cmd = cmd;
     }
     ~Command() {
+        // clean up file descriptors
         if (fd_in != 0) close(fd_in);
         if (fd_out != 1) close(fd_out);
-        if (fd_err != 2) close(fd_err);
     }
-    void parse() noexcept(false) {
+    void parse() {
         vector<string> _argv = tokenize(cmd, ' ');
         for (int i = 0; i < int(_argv.size()); i++) {
             if (_argv[i] == "&") {
-                if (i != argc - 1)
-                    throw invalid_argument("& must be the last argument");
                 bg = true;
             } else if (_argv[i] == "<") {
-                if (i == argc - 1)
-                    throw invalid_argument("< must be followed by a file name");
-                if (fd_out != 1)
-                    throw invalid_argument("< must be the first redirection");
-                else {
-                    fd_in = open(_argv[i + 1].c_str(), O_RDONLY);
-                    if (fd_in == -1)
-                        throw runtime_error("cannot open input file");
-                }
+                fd_in_name = _argv[i + 1];
                 i++;
             } else if (_argv[i] == ">") {
-                if (i == argc - 1)
-                    throw invalid_argument("> must be followed by a file name");
-                fd_out = open(_argv[i + 1].c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0700);
-                // fd_err = dup(fd_out); // TODO: should errstream be redirected to outstream?
-                if (fd_out == -1)
-                    throw runtime_error("cannot open output file");
+                fd_out_name = _argv[i + 1];
                 i++;
             } else if (_argv[i].size() != 0) {
                 argv.push_back(_argv[i]);
@@ -82,10 +66,12 @@ class Command {
                 cmd += " ";
         }
     }
+
     friend ostream& operator<<(ostream& os, const Command& _cmd) {
         os << _cmd.cmd << " < " << _cmd.fd_in << " > " << _cmd.fd_out << (_cmd.bg ? " &" : "");
         return os;
     }
+
     vector<char*> c_str() {
         vector<char*> _args;
         for (int i = 0; i < argc; i++) {
@@ -96,23 +82,40 @@ class Command {
         _args.push_back(nullptr);
         return _args;
     }
+
+    void open_fds() {
+        if (fd_in_name != "") {
+            fd_in = open(fd_in_name.c_str(), O_RDONLY);
+            if (fd_in == -1) {
+                perror("open");
+                exit(1);
+            }
+            dup2(fd_in, STDIN_FILENO);
+        }
+        if (fd_out_name != "") {
+            fd_out = open(fd_out_name.c_str(), O_WRONLY | O_CREAT | O_TRUNC, 0700);
+            if (fd_out == -1) {
+                perror("open");
+                exit(1);
+            }
+            dup2(fd_out, STDOUT_FILENO);
+        }
+    }
+
     void set_fd_in(int fd) {
         if (fd_in != 0) close(fd_in);
         fd_in = fd;
     }
+
     void set_fd_out(int fd) {
         if (fd_out != 1) close(fd_out);
         fd_out = fd;
     }
 };
+typedef pair<int, int> job_t;  // <pid,status>
 
-pid_t cpid;
-pid_t gpid;
-vector<int> bg_pids;  // background pids
-vector<int> fg_pids;  // foreground pids
-
+map<int, job_t> bg_jobs;
 vector<Command*> cmds;
-int cmd_idx = 0;
 int num_cmds = 0;
 
 void prompt(string& inp) {
@@ -120,7 +123,6 @@ void prompt(string& inp) {
         delete cmds[i];
     }
     cmds.clear();
-    cmd_idx = 0;
     num_cmds = 0;
 
     char buff[PATH_MAX];
@@ -145,15 +147,8 @@ void prompt(string& inp) {
     num_cmds = cmds.size();
 }
 
-void spawn_process(Command* cmd, int fdin, int fdout,
-                   int pgid) {
-    setpgid(0, pgid);
-    cpid = 
-}
-
 int main() {
     string inp;
-    gpid = cpid = getpid();  // get subgroup id
 
     struct sigaction sig_act;
     sig_act.sa_handler = SIG_IGN;
@@ -174,98 +169,90 @@ int main() {
         Command* cmd_begin = cmds[0];
         Command* cmd_end = cmds[num_cmds - 1];
 
-        for (Command* cmd : cmds) {
-            cout << *cmd << endl;
+        if (cmd_begin->cmd == "exit") {
+            break;
         }
+        // if (cmd_begin->cmd == "jobs") {
+        //     for (int i = 0; i < int(bg_pids.size()); i++) {
+        //         cout << "[" << i << "] " << bg_pids[i] <<
+        //     }
+        //     continue;
+        // }
 
-        int fgpid = 0;
-        int pipefd[2];
-        int in = cmd_begin->fd_in;
+        int fpgid = 0;
+        int in = 0;
+        pid_t pid = fork();
+        if (pid == 0) {
+            signal(SIGINT, SIG_DFL);
+            signal(SIGTSTP, SIG_DFL);
 
-        for (int i = 0; i < num_cmds - 1; i++) {
-            pipe(pipefd);
-            pid_t pid = fork();
-            if (pid == 0) {
-                Command* curr = cmds[i];
-                dup2(curr->fd_out, pipefd[1]);
-                dup2(curr->fd_in, in);
-                if (i == 0)
-                    setpgrp();
-                else
-                    setpgid(getpid(), fgpid);
+            Command* cmd = cmd_begin;
+            cmd->open_fds();  // io redirection
+            setpgrp();
+            tcsetpgrp(STDIN_FILENO, getpgrp());
 
-                signal(SIGINT, SIG_DFL);
-                signal(SIGTSTP, SIG_DFL);
-
-                auto args = cmds[i]->c_str();
-                execvp(args[0], &args[0]);
-                perror("execvp");
-                exit(1);  // exec error
-            } else {
-                if (i == 0) {
-                    fgpid = pid;
-                    tcsetpgrp(STDIN_FILENO, pid);
-                }
-                fg_pids.push_back(pid);
-                setpgid(pid, fgpid);
-                in = pipefd[0];
+            auto args = cmd->c_str();
+            execvp(args[0], &args[0]);
+            perror("execvp");
+            exit(1);
+        } else {
+            setpgid(pid, pid);
+            tcsetpgrp(STDIN_FILENO, getpgrp());
+            if (!cmd_begin->bg) {
+                waitpid(pid, NULL, WUNTRACED);
             }
         }
-
-        bool flag = 1;
-        for (int i = 0; i < num_cmds - 1; i++) {
-            if (cmds[i]->fd_out != 1) {
-                cout << "Error: " << cmds[i]->cmd << ": redirection error" << endl;
-                flag = 0;
-                break;
-            }
-            if (cmds[i + 1]->fd_in != 0) {
-                cout << "Error: " << cmds[i + 1]->cmd << ": redirection error" << endl;
-                flag = 0;
-                break;
-            }
-            p[0] = cmds[i + 1]->fd_in;
-            p[1] = cmds[i]->fd_out;
-            if (pipe(p) == -1) {
-                perror("pipe");
-                exit(EXIT_FAILURE);
-            }
-        }
-        if (!flag) {
-            continue;
-        }
-        int fgpid = getpid();
-        for (int i = 0; i < num_cmds; i++) {
-            cout << "cmd: " << cmds[i]->cmd << endl;
-            pid_t pid = fork();
-            if (pid == 0) {
-                if (i == 0) {
-                    setpgrp();
-                } else
-                    setpgid(getpid(), fgpid);
-
-                signal(SIGINT, SIG_DFL);
-                signal(SIGTSTP, SIG_DFL);
-
-                auto args = cmds[i]->c_str();
-                dup2(cmds[i]->fd_in, STDIN_FILENO);
-                dup2(cmds[i]->fd_out, STDOUT_FILENO);
-                execvp(args[0], &args[0]);
-
-                perror("execvp");
-                exit(1);  // exec error
-            } else {
-                if (i == 0) {
-                    fgpid = pid;
-                    tcsetpgrp(STDIN_FILENO, pid);
-                }
-                fg_pids.push_back(pid);
-                setpgid(pid, fgpid);
-            }
-        }
-        waitpid(-fgpid, NULL, WUNTRACED);
         tcsetpgrp(STDIN_FILENO, getpid());
 
+        // for (int i = 0; i < num_cmds - 1; i++) {
+        //     pipe(pipefd);
+        //     pid_t pid = fork();
+        //     if (pid == 0) {  // child
+        //         Command* curr = cmds[i];
+        //         curr->open_fds();
+        //         dup2(curr->fd_out, pipefd[1]);
+        //         dup2(curr->fd_in, in);
+        //         if (i == 0)
+        //             setpgrp();
+        //         else
+        //             setpgid(getpid(), fpgid);
+
+        //         signal(SIGINT, SIG_DFL);
+        //         signal(SIGTSTP, SIG_DFL);
+
+        //         auto args = cmds[i]->c_str();
+        //         execvp(args[0], &args[0]);
+        //         perror("execvp");
+        //         exit(1);  // exec error
+        //     } else {
+        //         if (i == 0) {
+        //             fpgid = pid;
+        //             tcsetpgrp(STDIN_FILENO, pid);
+        //         }
+        //         fg_pids.push_back(pid);
+        //         setpgid(pid, fpgid);
+        //         in = pipefd[0];
+        //     }
+        // }
+        // pid_t pid = fork();
+        // if (pid == 0) {
+        //     dup2(cmd_end->fd_in, in);
+        //     dup2(cmd_end->fd_out, STDOUT_FILENO);
+        //     setpgid(getpid(), fpgid);
+        //     signal(SIGINT, SIG_DFL);
+        //     signal(SIGTSTP, SIG_DFL);
+
+        //     auto args = cmd_end->c_str();
+        //     execvp(args[0], &args[0]);
+        //     perror("execvp");
+        //     exit(1);  // exec error
+        // } else {
+        //     fg_pids.push_back(pid);
+        //     setpgid(pid, fpgid);
+        // }
+
+        // waitpid(-fpgid, NULL, WUNTRACED);
+        tcsetpgrp(STDIN_FILENO, getpid());
         // int pipe[2];
         // for (Command* cmd : cmds) {
         // }
