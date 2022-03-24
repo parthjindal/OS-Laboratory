261bb2ea68ba2ab3dfe9f7cff968761f0ce6f5a7
Parth Jindal <pmjindal@gmail.com>
2022-03-24 14:39:30 +0530

    debug functions

--- Assignment-5/memlab.cc
+++ Assignment-5/memlab.cc
@@ -27,7 +27,7 @@ class Type {
     TypeEnum type;
     Type(TypeEnum _type) : type(_type) {}
     Type(const Type& other) : type(other.type) {}
-    virtual ~Type() {}
+    ~Type() {}
 };
 
 class ArrType : public Type {
@@ -69,9 +69,9 @@ struct Symbol {
     unsigned int word1, word2;
 };
 
-#define MAX_SYMBOLS 1024
+#define MAX_SYMBOLS 3
 struct SymbolTable {
-    int head, tail;
+    unsigned int head, tail;
     Symbol symbols[MAX_SYMBOLS];
     int size;
     pthread_mutex_t mutex;
@@ -82,39 +82,50 @@ struct SymbolTable {
         // Explicit free list
         for (int i = 0; i < MAX_SYMBOLS; i++) {
             symbols[i].word1 = 0;
-            symbols[i].word2 = ((i + 1) * sizeof(Symbol)) << 1;
+            symbols[i].word2 = ((i + 1)) << 1;
         }
         symbols[tail].word2 = -2;  // mark end of free list
         pthread_mutexattr_t attr;
         pthread_mutexattr_init(&attr);
         pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK_NP);
         pthread_mutex_init(&mutex, &attr);
+        // cout << "SymbolTable initialized" << endl;
     }
-    int alloc(int wordidx, int offset) {
+    unsigned int alloc(unsigned int wordidx, unsigned int offset) {
         pthread_mutex_lock(&mutex);
         if (size == MAX_SYMBOLS) {
             pthread_mutex_unlock(&mutex);
             return -1;
         }
-        int idx = head;
-        head = symbols[head].word2 & -2;
+        // DEBUG()
+        cout << "Allocating symbol " << wordidx << ":" << offset << endl;
+        unsigned int idx = head;
+        head = (symbols[head].word2 & -2) >> 1;
         symbols[idx].word1 = (wordidx << 1) | 1;  // mark as allocated
         symbols[idx].word2 = (offset << 1) | 1;   // mark as in use
         size++;
         pthread_mutex_unlock(&mutex);
         return idx;  // local address
     }
-    void free(int idx) {
+    void free(unsigned int idx) {
         pthread_mutex_lock(&mutex);
+        if (size == MAX_SYMBOLS) {
+            head = tail = idx;
+            symbols[idx].word1 = 0;
+            symbols[idx].word2 = -2;  // sentinel
+            size--;
+            pthread_mutex_unlock(&mutex);
+            return;
+        }
         symbols[tail].word2 = idx << 1;
         symbols[idx].word1 = 0;
-        symbols[idx].word2 = -2;
+        symbols[idx].word2 = -2;  // sentinel
         tail = idx;
         size--;
         pthread_mutex_unlock(&mutex);
     }
-    int getWordIdx(int idx) { return symbols[idx].word1 >> 1; }
-    int getOffset(int idx) { return symbols[idx].word2 >> 1; }
+    int getWordIdx(unsigned int idx) { return symbols[idx].word1 >> 1; }
+    int getOffset(unsigned int idx) { return symbols[idx].word2 >> 1; }
 };
 
 SymbolTable* symTable;
@@ -210,14 +221,18 @@ void createMem(int size) {
     stack->Init();
 }
 
+int translate(int local_addr) {
+    return local_addr << 2;
+}
+
 Ptr createVar(const Type& t) {
     int _size = getSize(t);
-    _size = (((_size + 3) >> 2) << 2) + 8;
+    _size = (((_size + 3) >> 2) << 2);
     int wordid = mem->getMem(_size);
     if (wordid == -1)
         throw std::runtime_error("Out of memory");
     int local_addr = symTable->alloc(wordid, 0);
-    return Ptr(t, local_addr);
+    return Ptr(t, translate(local_addr));
 }
 
 void assignVar(const Ptr& p, int val) {
@@ -245,24 +260,63 @@ void assignVar(const Ptr& p, bool f) {
 void assignVar(const Ptr& p) {
 }
 
-int main() {
+void testMemBlock() {
     MemBlock* mem = new MemBlock();
-    mem->Init(1024 * 1024);
+    mem->Init(1024 * 1024);  // 1 MB
     int word1 = mem->getMem(512);
     cout << "word1: " << word1 << endl;
     int word2 = mem->getMem(256);
     cout << "word2: " << word2 << endl;
     int word3 = mem->getMem(128);
     cout << "word3: " << word3 << endl;
-    // mem->freeBlock(word3);
-    // cout << "freeing word3" << endl;
-    // mem->freeBlock(word2);
-    // cout << "freeing word2" << endl;
     mem->freeBlock(word1);
     cout << "freeing word1" << endl;
-
-    word1 = mem->getMem(32);
-    cout << "word1: " << word1 << endl;
-    int word4 = mem->getMem(64);
+    int word4 = mem->getMem(128);
     cout << "word4: " << word4 << endl;
+    mem->freeBlock(word2);
+    cout << "freeing word2" << endl;
+    int word5 = mem->getMem(640);
+    cout << "word5: " << word5 << endl;
 }
+
+void testSymbolTable() {
+    SymbolTable* symtable = new SymbolTable();
+    symtable->Init();
+    cout << "here" << endl;
+    // cout << "alloc: " << symtable->alloc(0, 0) << endl;
+    int v1 = symtable->alloc(0, 0);
+    cout << "v1: " << v1 << endl;
+    int v2 = symtable->alloc(4, 0);
+    cout << "v2: " << v2 << endl;
+    int v3 = symtable->alloc(8, 0);
+    cout << "v3: " << v3 << endl;
+    symtable->free(v2);
+    symtable->free(v1);
+    int v4 = symtable->alloc(4, 0);
+    cout << "v4: " << v4 << endl;
+    int v5 = symtable->alloc(4, 0);
+    cout << "v5: " << v5 << endl;
+}
+
+void testCreateVar() {
+    createMem(1024 * 1024);
+    Ptr p1 = createVar(Type(Type::INT));
+    Ptr p2 = createVar(Type(Type::BOOL));
+    Ptr p3 = createVar(Type(Type::CHAR));
+    cout << p1.addr << endl;
+    cout << p2.addr << endl;
+    cout << p3.addr << endl;
+    
+}
+
+int main() {
+    // testSymbolTable();Type(Type::INT)
+    testCreateVar();
+}
+
+/**
+ * TODO LIST:
+ * 1. Write garbage collector and compaction
+ * 2.
+ *
+ */
