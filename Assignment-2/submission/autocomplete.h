7218d6642f0bfd4310ce3b294615dcce3f2f9310
Parth Jindal <pmjindal@gmail.com>
2022-04-01 17:11:46 +0530

    clean directory

--- Assignment-2/submission/autocomplete.h
+++
@@ -1,8 +0,0 @@
-#ifndef _AUTOCOMPLETE_H
-#define _AUTOCOMPLETE_H
-#include <deque>
-#include <string>
-#include <vector>
-
-std::vector<std::string> autocomplete(std::string inp);
-#endif  // _AUTOCOMPLETE_H
